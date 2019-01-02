c0dd7c36890bb2e4d222a3088120ee4b162252cf
yangtao <18111987@suningcultural.com>
2019-01-02 18:16:40 +0800

    test

---
+++ src/main/java/com/pp/server/EMoniterApplication.java
@@ -0,0 +1,14 @@
+package com.pp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMoniterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMoniterApplication.class, args);
+	}
+
+}
+
